735d3e81a874d105f1a761e750530e3d0ab855f0
KEITA135 <ikeisakurasaku@gmail.com>
2025-09-02 00:27:47 +0900

    /dashboardのlayoutにSuspense追加

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { Suspense } from 'react';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,7 +8,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <SideNav />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-        {children}
+        <Suspense>
+          {children}
+        </Suspense>
       </div>
     </div>
   );
